7f5ce8282c8cd21d33aeb1094b87647deb7729d3
amari <amari.lawal@gmail.com>
2024-02-10 11:34:32 +0000

    CaesarAI Template Cloud Run

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "google_cloud_run_service" "run_service" {
   template {
     spec {
       containers {
-        image = "palondomus/caesaraifastapi:2"
+        image = "palondomus/caesaraifastapi:3"
       }
     }
   }
